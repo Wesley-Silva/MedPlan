628763c3f8d17496e2af5f7a3bd49afd82717a28
Wesley-Silva <wesleybf3@gmail.com>
2021-07-09 23:59:21 -0300

    Adicionado a Area-Identity

---
+++ MedPlan/MedPlan.App/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using MedPlan.App.Data;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(MedPlan.App.Areas.Identity.IdentityHostingStartup))]
+namespace MedPlan.App.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
